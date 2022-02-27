1d403060ca72989132548b701c7bc7b61a288d3d
gunic <gunichachacha@gmail.com>
2022-02-27 16:16:21 +0500

    изменил название файла

---
+++ 3.js
@@ -0,0 +1,12 @@
+function spinWords(string)
+{
+    
+}
+
+
+
+const result1 = spinWords( "Привет от Legacy" )
+console.log(result1) // тевирП от ycageL
+
+const result2 = spinWords( "This is a test" )
+console.log(result2) // This is a test
