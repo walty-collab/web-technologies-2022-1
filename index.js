9e8e326bd0254393b82ba4c64f9768e37fd0c985
Pavel Potemkin <p.potemkin@legacystudio.ru>
2022-03-10 12:01:20 +0500

    Revert "Практика 4 - крестики нолики"
    This reverts commit 2093a76a

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import ListItems from "./components/ListItems.js";
+import ListItems from "./components/list-items.js";
 
 if (document.readyState === 'loading') {
   document.addEventListener('DOMContentLoaded', init)
